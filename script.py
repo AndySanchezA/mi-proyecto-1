f3c26f4abb06abcadb80961dbb6c26ce807ea2dd
SVINFYLKING <isacorecal@outlook.com>
2021-01-04 14:44:21 -0500

    Agregado datos del proyecto

--- script.py
+++ script.py
@@ -2,7 +2,10 @@
 # Autor: Rodrigo Tufiño <rtufino@ups.edu.ec>
 # Fecha: 23-06-2020
 # Versión: 1.0.0
+
 def main():
-print("Hola Mundo!")
+	nombre = input("Cual es tu nombre?")
+	print("Hola", nombre, "Bienvenido a Git")
+
 if __name__ == "__main__":
-main()
+	main()
